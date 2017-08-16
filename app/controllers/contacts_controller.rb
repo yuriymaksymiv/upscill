9d4f11836104018effbfd68cfb8490cc0ba68a72
Maks <maksymiv.yura@gmail.com>
2017-08-16 11:48:20 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,18 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
+  end
+  
+  def create    
+      @contact = Contact.new(contact_params)
+      if @contact.save
+          redirect_to new_contact_path, notice: "Messege send."
+      else
+          redirect_to new_contact_path, notice: "Message occured."
+  end
+  end
+  private
+  def contact_params
+     params.require(:contact).permit(:name, :email, :comments)
   end
 end
